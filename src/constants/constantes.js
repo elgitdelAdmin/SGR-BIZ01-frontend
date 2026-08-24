2c933f63da9c58742e7188df0163191f38ca8fb2
ecamarenacsticorpbiz <edgarcamarena60@gmail.com>
2026-08-24 15:45:43 -0500

    apuntar a prod

--- src/constants/constantes.js
+++ src/constants/constantes.js
@@ -1,6 +1,6 @@
-//export const URLAPICONECTA = "http://154.38.177.31:5000";                             /* Producción */
+export const URLAPICONECTA = "http://154.38.177.31:5000";                             /* Producción */
 //export const URLAPICONECTA = "http://154.38.177.31:5001";                           /* Qa */
 //export const URLAPICONECTA = "https://localhost:44309";                             /* Local */
 
 //export const URLAPICONECTA = "http://localhost:5001";                           /* QA Local */
-export const URLAPICONECTA = "http://localhost:5000";                           /* Producción Local */
+//export const URLAPICONECTA = "http://localhost:5000";                           /* Producción Local */
